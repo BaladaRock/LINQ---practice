b95364ec760c7685bf1e410b9e1c2bdfe95df48e
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-10-16 21:07:49 +0300

    NumberOperations - implemented GenerateSubsets

---
+++ LINQ_applications/NumberOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_applications
+{
+    public class NumberOperations
+    {
+        public static IEnumerable<IEnumerable<int>> GenerateSubsets(int[] array, int maxSum = int.MinValue)
+        {
+            ThrowNullException(array);
+
+            return array.SelectMany(
+                (start, finish) =>
+                    GetSubSet(array, start, finish)
+                    .Select((x, index) => GetSubSet(array, start, array.Length - index - start)))
+               .Where(x => CheckSumOfElements(x, maxSum));
+        }
+
+        private static bool CheckSumOfElements(IEnumerable<int> array, int sum)
+        {
+            return array.Aggregate(0, (x, y) => x + y) <= sum;
+        }
+
+        private static IEnumerable<int> GetSubSet(IEnumerable<int> array, int startingPosition, int numbersToTake)
+        {
+            return array.Skip(startingPosition).Take(numbersToTake);
+        }
+
+        private static void ThrowNullException(IEnumerable<int> array)
+        {
+            if (array != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(nameof(array));
+        }
+    }
+}
---
+++ LINQ_applications_Facts/NumberFacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using LINQ_applications;
+
+namespace LINQ_applications_Facts
+{
+    public class NumberFacts
+    {
+        [Fact]
+        public void Test_GenerateSubsets_Simple_Case()
+        {
+            //Given
+            int[] array = { 1, 2 };
+            //When
+            var subSets = NumberOperations.GenerateSubsets(array, 1);
+            //Then
+            Assert.Equal(new[] { new int[] { 1 } }, subSets);
+        }
+
+    }
+}
