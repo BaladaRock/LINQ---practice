a664a8578d6bc4d20b9907966bd3e845027ef182
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-10-20 02:53:11 +0300

    NumberOperations - GetTriplePermutations has been added to GetPythagoreanNumbers()

--- LINQ_applications/NumberOperations.cs
+++ LINQ_applications/NumberOperations.cs
@@ -6,25 +6,42 @@ namespace LINQ_applications
 {
     public class NumberOperations
     {
-        public static IEnumerable<IEnumerable<int>> GenerateSubsets(int[] array, int maxSum = int.MinValue)
+        public static IEnumerable<IEnumerable<int>> GenerateSubsets(IEnumerable<int> array, int maxSum = int.MinValue)
         {
             ThrowNullException(array);
 
             return array.SelectMany((_, startPos) => GetSubSet(array, startPos)
                                     .Select((number, secIndex)
-                                    => GetSubSet(array, startPos, array.Length - secIndex - startPos)))
+                                    => GetSubSet(array, startPos, array.Count() - secIndex - startPos)))
                         .Where(x => CheckSumOfElements(x, maxSum));
         }
 
-        public static IEnumerable<IEnumerable<int>> GetPythagoreanNumbers(int[] array)
+        public static IEnumerable<IEnumerable<int>> GetPythagoreanNumbers(IEnumerable<int> array)
         {
             ThrowNullException(array);
 
-            var triples = array.SelectMany((first, fstIndex) => array.Skip(fstIndex + 1)
-                                          .SelectMany((sec, secIndex) => array.Skip(fstIndex + ++secIndex + 1)
-                                                     .Select(third => new[] { first, sec, third })));
+            return array.SelectMany((first, fstIndex) => array.Skip(fstIndex + 1)
+                                   .SelectMany((sec, secIndex) => array.Skip(fstIndex + ++secIndex + 1)
+                                              .Select(third => new[] { first, sec, third })))
+                        .SelectMany(combination => IsTriplePythagorean(combination)
+                                              ? GetTriplePermutations(combination).Select(x => x)
+                                              : combination);
+        }
 
-            return SelectPythagoreanCombinations(triples);
+        private static bool IsTriplePythagorean(IEnumerable<int> triple)
+        {
+            int firstSquare = triple.First() * triple.First();
+            int secondSquare = triple.ElementAt(1) * triple.ElementAt(1);
+            int thirdSquare = triple.Last() * triple.Last();
+
+            if (thirdSquare >= firstSquare && thirdSquare >= secondSquare)
+            {
+                return firstSquare + secondSquare == thirdSquare;
+            }
+
+            return firstSquare >= secondSquare
+                ? firstSquare + thirdSquare == secondSquare
+                : secondSquare + thirdSquare == firstSquare;
         }
 
         private static bool CheckSumOfElements(IEnumerable<int> array, int sum)
@@ -42,9 +59,18 @@ namespace LINQ_applications
             return array.Skip(startingPosition);
         }
 
-        private static IEnumerable<IEnumerable<int>> SelectPythagoreanCombinations(IEnumerable<int[]> triples)
+        private static IEnumerable<IEnumerable<int>> GetTriplePermutations(IEnumerable<int> triple)
         {
-            throw new NotImplementedException();
+            int first = triple.First();
+            int second = triple.ElementAt(1);
+            int last = triple.Last();
+
+            var elements = triple.ToList();
+            elements.Sort();
+
+            return last >= first
+                ? new[] { new[] { first, second, last }, new[] { second, first, last } }
+                : new[] { new[] { last, first, second }, new[] { last, second, first } };
         }
 
         private static void ThrowNullException(IEnumerable<int> array)
