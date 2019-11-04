6f3a9d9d6f82390eb57c452e97148595f290110d
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-11-04 17:51:45 +0200

    WordsApparitions - top most used words from a string[]

---
+++ LINQ_applications/WordsApparitions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_applications_Facts
+{
+    public class WordsApparitions
+    {
+        public WordsApparitions(string[] text)
+        {
+            Text = text;
+        }
+
+        public IEnumerable<string> Text { get; set; }
+
+        public IEnumerable<string> GetMostUsed()
+        {
+            return Text.GroupBy(x => x.ToLower())
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Key);
+        }
+    }
+}
